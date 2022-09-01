b8832ed51485a1c56b27bc6dd860b2afbcbdbe75
martin-pedraza <pedraza.martindiego@gmail.com>
2022-09-01 11:53:17 -0300

    conectando login/registro al home

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-app.get('/home', (req, res)=>{
-    res.sendFile((__dirname + '/views/home.html'));
-});
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("servidor arriba");
+app.listen(process.env.PORT || 3005, function(){
+    console.log("corriendo en puerto 3005...");
 })
+
+app.get('/', (req, res)=>{
+    res.sendFile((__dirname + '/views/home.html'));
+});
+
+app.get('/registro', (req, res)=>{
+    res.sendFile((__dirname + '/views/registro.html'));
+});
+
+app.get('/password', (req, res)=>{
+    res.sendFile((__dirname + '/views/password.html'));
+});
+
+app.get('/login', (req, res)=>{
+    res.sendFile((__dirname + '/views/login.html'));
+});
