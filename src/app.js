42efbbecffc4b3d71b8d29c6d61e889b295e1d18
martin-pedraza <pedraza.martindiego@gmail.com>
2022-09-15 13:08:29 -0300

    imagenes

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ const errorRoutes = require('./routes/errorRoutes');
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.resolve(__dirname, './../public')));
+
 app.use('/', mainRoutes);
 
 app.use('/users', usersRoutes);
@@ -17,8 +19,6 @@ app.use('/users', usersRoutes);
 app.use('/products', productsRoutes);
 
 app.use('*', errorRoutes);
-
-app.use(express.static(path.resolve(__dirname, './public')));
 
 app.listen(port, function(){
     console.log("corriendo en puerto 3005...");
