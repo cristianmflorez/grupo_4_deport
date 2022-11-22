6f5edb6a38408286361b8d14b08a789a7f390177
martin-pedraza <pedraza.martindiego@gmail.com>
2022-11-21 21:38:19 -0300

    probando heroku2

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
     "username": "289492_dscc",
     "password": "Vea0lre289911",
     "database": "danielcorrea28_mydb",
     "host": "mysql-danielcorrea28.alwaysdata.net",
     "dialect": "mysql",
     "port": 3306
-  }
 }
