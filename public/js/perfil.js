341d157631e5e7f70bbe47bde6bc24f2e97807c2
cristianmflorez <cristianmflorez@gmail.com>
2022-12-12 10:13:21 -0500

    primera version valid front

---
+++ public/js/perfil.js
@@ -0,0 +1,37 @@
+let nombre = document.getElementById('nombre');
+let correo = document.getElementById('correo');
+let telefono = document.getElementById('telefono');
+let direccion = document.getElementById('direccion');
+
+let formulario = document.getElementById('formulario');
+
+let arreglo = [];
+
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    if(correo.value.length<5 || !correo.value.includes('@') || !correo.value.slice(correo.value.indexOf('@'), -2).includes('.')){
+        swal("Debes introducir un correo válido", "", "warning");
+        return;
+    } 
+
+    if(nombre.value.length<1){
+        arreglo.push("Nombre");
+    }
+
+    if(telefono.value.length<7){
+        arreglo.push("Teléfono");
+    }
+
+    if(direccion.value.length<1){
+        arreglo.push("Dirección");
+    }
+
+    if(arreglo.length>0){
+        swal("Debes diligenciar correctamente los campos: "+arreglo, "", "warning");
+        arreglo = [];
+        return;
+    }
+
+    formulario.submit();
+})
---
+++ public/js/registro.js
@@ -0,0 +1,49 @@
+let nombre = document.getElementById('nombre');
+let correo = document.getElementById('correo');
+let password = document.getElementById('password');
+let passwordConfirm = document.getElementById('passwordConfirm');
+let telefono = document.getElementById('telefono');
+let direccion = document.getElementById('direccion');
+let pais = document.getElementById('pais');
+let avatar = document.getElementById('avatar');
+
+let formulario = document.getElementById('formulario');
+
+let arreglo = [];
+
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    if(correo.value.length<5 || !correo.value.includes('@') || !correo.value.slice(correo.value.indexOf('@'), -2).includes('.')){
+        swal("Debes introducir un correo válido", "", "warning");
+        return;
+    } 
+
+    if(nombre.value.length<1){
+        arreglo.push("Nombre");
+    }
+
+    if(password.value.length<6){
+        arreglo.push("Contraseña");
+    }
+
+    if(passwordConfirm.value.length<6 || passwordConfirm.value!==password.value){
+        arreglo.push("Confirmar contraseña");
+    }
+
+    if(telefono.value.length<7){
+        arreglo.push("Teléfono");
+    }
+
+    if(direccion.value.length<1){
+        arreglo.push("Dirección");
+    }
+
+    if(arreglo.length>0){
+        swal("Debes diligenciar correctamente los campos: "+arreglo, "", "warning");
+        arreglo = [];
+        return;
+    }
+
+    formulario.submit();
+})
